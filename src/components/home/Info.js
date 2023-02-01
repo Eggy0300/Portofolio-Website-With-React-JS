90ef70ec7f873b2a1a9dd5dc606134d1720ee1da
Eggy0300 <eggy.egong@gmail.com>
2023-02-01 13:21:34 +0700

    some changes

--- src/components/home/Info.js
+++ src/components/home/Info.js
@@ -4,7 +4,7 @@ const Info = () => {
     return (
         <div className="home_info">
             <h1 className="home_title">Gregorius Agung Nugroho</h1>
-            <h3 className="home_subt">Backend Developer & Data Engineer</h3>
+            <h3 className="home_subt">Web Developer</h3>
             <p className="home_desc">
                 I'm a Backend Developer based in Tangerang, Indonesia. I'm an enthusiastic person who can learn quickly and easy to adapt, also dedicated to my work.
             </p>
