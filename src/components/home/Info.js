f6daac5f9e8fea3ade7075ad64536cc52f14ea28
Eggy0300 <eggy.egong@gmail.com>
2023-02-16 14:47:43 +0700

    Update Info.js

--- src/components/home/Info.js
+++ src/components/home/Info.js
@@ -3,7 +3,7 @@ import React from "react";
 const Info = () => {
     return (
         <div className="home_info">
-            <h1 className="home_title">Gregorius Agung Nugroho Kevin anjing</h1>
+            <h1 className="home_title">Gregorius Agung Nugroho...</h1>
             <h3 className="home_subt">Web Developer</h3>
             <p className="home_desc">
                 I'm a Web Developer based in Tangerang, Indonesia. I'm an enthusiastic person who can learn quickly and easy to adapt, also dedicated to my work.
